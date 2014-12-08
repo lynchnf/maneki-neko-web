b4c6df6d153fe16bfc491384590d65da5a2ce24e
Norman Lynch <lynchnf@gmail.com>
2014-12-08 14:13:37 -0600

    Added place holder for test case for social link plugin.

--- socialmedia/tests.py
+++ socialmedia/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
-# Create your tests here.
+class SocialLinkPluginTest(TestCase):
+    
+    def test_render(self):
+        self.fail("Need to write tests for this plugin.")
