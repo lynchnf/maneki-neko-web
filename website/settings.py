07015f77593be8138b32114c52e65c6cc0b2b38e
Norman Lynch <lynchnf@gmail.com>
2014-12-18 15:17:45 -0600

    Set up logging.

--- website/settings.py
+++ website/settings.py
@@ -182,3 +182,42 @@ DATABASES = {
     'default':
         {'ENGINE': 'django.db.backends.mysql', 'NAME': u'mnwebdb', 'HOST': u'localhost', 'USER': u'mnwebuser', 'PASSWORD': u'swordfish', 'PORT': 3306}
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'console_fmt': {
+            'format': '%(levelname)-7s | %(name)s | %(message)s'
+        },
+        'file_fmt': {
+            'format': '%(processName)s %(asctime)s | %(name)s | %(levelname)-7s | %(message)s'
+        }                   
+    },
+    'handlers': {
+        'console': {
+            'formatter': 'console_fmt',
+            'class': 'logging.StreamHandler'
+        },
+        'file': {
+            'formatter': 'file_fmt',
+            'class': 'logging.handlers.WatchedFileHandler',
+            'filename': '/var/log/maneki-neko-con/website.log'
+        }
+    },
+    'loggers': {
+        'socialmedia': {
+            'level': 'DEBUG',
+            'handlers': ['console','file']
+        },
+        'website': {
+            'level': 'DEBUG',
+            'handlers': ['console','file']
+        },
+# Everything else goes to the root logger.                
+        'website': {
+            'level': 'INFO',
+            'handlers': ['console','file']
+        }
+    }
+}
