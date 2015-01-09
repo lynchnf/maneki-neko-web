f486318bb560abc6cc6e6901a5a6d25f872085d4
Norman Lynch <lynchnf@gmail.com>
2015-01-08 20:04:39 -0600

    Smart crop in gallery.

--- website/settings.py
+++ website/settings.py
@@ -197,4 +197,12 @@ CMS_PLACEHOLDER_CONF = {
     }
 }
 
+THUMBNAIL_PROCESSORS = (
+    'easy_thumbnails.processors.colorspace',
+    'easy_thumbnails.processors.autocrop',
+    #'easy_thumbnails.processors.scale_and_crop',
+    'filer.thumbnail_processors.scale_and_crop_with_subject_location',
+    'easy_thumbnails.processors.filters',
+)
+
 LOGGING = env_settings.LOGGING
