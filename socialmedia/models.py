24ad2a950b12d520b48ba18ff46236cb1f44b351
Norman Lynch <lynchnf@gmail.com>
2015-01-26 19:30:37 -0600

    Make social media icons pretty colors.

--- socialmedia/models.py
+++ socialmedia/models.py
@@ -7,31 +7,24 @@ ICON_CHOICES = (
         ("fa-delicious", _("Delicious")),
         ("fa-digg", _("Digg")),
         ("fa-facebook", _("Facebook")),
-        ("fa-facebook-square", _("Facebook (square)")),
         ("fa-flickr", _("Flickr")),
-        ("fa-foursquare", _("Foursquare")),
         ("fa-google-plus", _("Google+")),
-        ("fa-google-plus-square", _("Google+ (square)")),
         ("fa-instagram", _("Instagram")),
+        ("fa-linkedin", _("LinkedIn")),
+        ("fa-map-marker", _("Map")),
         ("fa-pinterest", _("Pinterest")),
-        ("fa-pinterest-square", _("Pinterest (square)")),
-        ("fa-reddit", _("Reddit")),
-        ("fa-reddit-square", _("Reddit (square)")),
+        ("fa-rss", _("RSS feed")),
+        ("fa-reddit", _("reddit")),
         ("fa-spotify", _("Spotify")),
         ("fa-stumbleupon", _("StumbleUpon")),
-        ("fa-stumbleupon-circle", _("StumbleUpon (circle)")),
         ("fa-tumblr", _("Tumblr")),
-        ("fa-tumblr-square", _("Tumblr (square)")),
         ("fa-twitter", _("Twitter")),
-        ("fa-twitter-square", _("Twitter (square)")))
+        ("fa-youtube-play", _("YouTube")))
 
 class SocialLink(CMSPlugin):
     icon = models.CharField("Social Network Icon", max_length=20, choices=ICON_CHOICES)
     size = models.IntegerField("Icon Size", default=0)
     url = models.URLField("URL")
-    
-    def x__unicode__x(self):
-        return self.url
-    
+
     def __unicode__(self):
         return "icon is %s, size is %s, url is %s" % (self.icon, self.size, self.url)
