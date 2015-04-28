1d99e64cde71ffbd542aee5cdee88e4c30ac225f
Norman Lynch <lynchnf@gmail.com>
2015-04-28 15:58:25 -0500

    I have no idea what I done.

--- staff/cms_plugins.py
+++ staff/cms_plugins.py
@@ -2,6 +2,8 @@ from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from django.utils.translation import ugettext_lazy as _
 from staff.models import Department
+from django.contrib.auth.models import User
+
 import logging
 
 logger = logging.getLogger(__name__)
@@ -16,4 +18,15 @@ class ContactUsPlugin(CMSPluginBase):
         context['instance'] = instance
         return context
 
+class StaffListPlugin(CMSPluginBase):
+    name = _("Staff List")
+    render_template = "staff_list.html"
+
+    def render(self, context, instance, placeholder):
+        users = User.objects.all()
+        context['users'] = users
+        context['instance'] = instance
+        return context
+
 plugin_pool.register_plugin(ContactUsPlugin)
+plugin_pool.register_plugin(StaffListPlugin)
